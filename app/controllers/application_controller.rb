f0d885d9ebfc0f2e9ac8823c3d8ef105d54288e3
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-05 20:07:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
 
-    if user.save
+    if user != nil
         redirect "/login"
     else
         redirect "/failure"
